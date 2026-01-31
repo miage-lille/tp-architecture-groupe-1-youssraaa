6a13249b2c1b291b7897b64da8c33da1080fec2b
youssraaa <74369761+youssraaa@users.noreply.github.com>
2026-01-31 19:29:01 +0100

    feat(webinars): add in-memory participation repository

---
+++ src/webinars/adapters/participation-repository.in-memory.ts
@@ -0,0 +1,16 @@
+import { Participation } from 'src/webinars/entities/participation.entity';
+import { IParticipationRepository } from 'src/webinars/ports/participation-repository.interface';
+
+export class InMemoryParticipationRepository implements IParticipationRepository {
+  constructor(private readonly database: Participation[] = []) {}
+
+  async findByWebinarId(webinarId: string): Promise<Participation[]> {
+    return this.database.filter(
+      (participation) => participation.props.webinarId === webinarId,
+    );
+  }
+
+  async save(participation: Participation): Promise<void> {
+    this.database.push(participation);
+  }
+}
